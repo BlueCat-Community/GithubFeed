32e5159555466681ea9a666c4217900f87d0e46b
rurimo <topmazino@gmail.com>
2019-10-07 21:35:48 +0900

    Implement basic core based on mvp architecture

---
+++ core/src/main/java/com/bluecat/core/ActivityPresenterManager.kt
@@ -0,0 +1,116 @@
+/*
+ * Copyright 2019 Team Mulro in BlueCat-Community
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bluecat.core
+
+import android.content.Context
+import android.os.Bundle
+import java.lang.reflect.InvocationTargetException
+import java.util.UUID
+import kotlin.collections.HashMap
+import kotlin.reflect.KClass
+import kotlin.reflect.full.createInstance
+
+@Suppress("UNCHECKED_CAST")
+class ActivityPresenterManager {
+    private val presenters = HashMap<String, BasePresenter<*>>()
+
+    fun <T : BasePresenter<*>> fetch(
+        context: Context,
+        presenterClass: Class<T>,
+        savedInstanceState: Bundle?
+    ): T {
+        val id = fetchId(savedInstanceState)
+        var activityPresenter: BasePresenter<*>? = this.presenters[id]
+
+        if (activityPresenter == null) {
+            activityPresenter = create(context, presenterClass.kotlin, savedInstanceState, id!!)
+        }
+
+        return activityPresenter as T
+    }
+
+    fun save(activityPresenter: BasePresenter<*>, envelope: Bundle) {
+        envelope.putString(PRESENTER_ID_KEY, findIdForPresenter(activityPresenter))
+        val state = Bundle()
+        envelope.putBundle(PRESENTER_STATE_KEY, state)
+    }
+
+    private fun <T : BasePresenter<*>> create(
+        context: Context,
+        presenterClass: KClass<T>,
+        savedInstanceState: Bundle?,
+        id: String
+    ): BasePresenter<*> {
+
+        val activityPresenter: BasePresenter<*>
+
+        try {
+            activityPresenter = presenterClass.createInstance()
+        } catch (exception: IllegalAccessException) {
+            throw RuntimeException(exception)
+        } catch (exception: InvocationTargetException) {
+            throw RuntimeException(exception)
+        } catch (exception: InstantiationException) {
+            throw RuntimeException(exception)
+        } catch (exception: NoSuchMethodException) {
+            throw RuntimeException(exception)
+        }
+
+        this.presenters[id] = activityPresenter
+        activityPresenter.onCreate(
+            context,
+            BundleUtils.maybeGetBundle(savedInstanceState, PRESENTER_STATE_KEY)
+        )
+        return activityPresenter
+    }
+
+    fun destroy(activityPresenter: BasePresenter<*>) {
+        activityPresenter.onDestroy()
+
+        val iterator = this.presenters.entries.iterator()
+        while (iterator.hasNext()) {
+            val entry = iterator.next()
+            if (activityPresenter == entry.value) {
+                iterator.remove()
+            }
+        }
+    }
+
+    private fun fetchId(savedInstanceState: Bundle?): String? {
+        return if (savedInstanceState != null)
+            savedInstanceState.getString(PRESENTER_STATE_KEY)
+        else
+            UUID.randomUUID().toString()
+    }
+
+    private fun findIdForPresenter(activityPresenter: BasePresenter<*>): String {
+        for ((key, value) in this.presenters) {
+            if (activityPresenter == value) {
+                return key
+            }
+        }
+
+        throw RuntimeException("cannot find presenter in map!")
+    }
+
+    companion object {
+        private const val PRESENTER_ID_KEY = "presenter_id"
+        private const val PRESENTER_STATE_KEY = "presenter_state"
+
+        val instance = ActivityPresenterManager()
+    }
+}
---
+++ core/src/main/java/com/bluecat/core/BaseActivity.java
@@ -0,0 +1,160 @@
+/*
+ * Copyright (C) 2016 skydoves
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bluecat.core;
+
+import android.os.Bundle;
+
+import androidx.annotation.CallSuper;
+import androidx.annotation.LayoutRes;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bluecat.core.qualifiers.RequirePresenter;
+
+public abstract class BaseActivity<Presenter extends BasePresenter, ViewType extends BaseView>
+        extends AppCompatActivity {
+
+    private static final String PRESENTER_KEY = "presenter";
+    protected Presenter presenter;
+    protected ViewType baseView;
+
+    /**
+     * get presenter
+     *
+     * @return presenter
+     */
+    public Presenter presenter() {
+        return presenter;
+    }
+
+    /**
+     * get baseView
+     *
+     * @return baseView
+     */
+    public ViewType baseView() {
+        return baseView;
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void setContentView(@LayoutRes int layoutResID) {
+        super.setContentView(layoutResID);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        if (isFinishing()) {
+            if (this.presenter != null) {
+                ActivityPresenterManager.Companion.getInstance().destroy(this.presenter);
+                this.presenter = null;
+            }
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        final Bundle presenterEnvelope = new Bundle();
+        if (this.presenter != null) {
+            ActivityPresenterManager.Companion.getInstance().save(this.presenter, presenterEnvelope);
+        }
+    }
+
+    /**
+     * get WDApplication
+     *
+     * @return {@link GithubFeedApplication} instance
+     */
+    protected @NonNull
+    GithubFeedApplication application() {
+        return (GithubFeedApplication) getApplication();
+    }
+
+    /**
+     * triggers a back press
+     */
+    public void goBack() {
+        super.onBackPressed();
+    }
+
+    /**
+     * initialize baseView on presenter
+     *
+     * @param viewType baseView
+     */
+    @CallSuper
+    protected void initBaseView(ViewType viewType) {
+        this.baseView = viewType;
+
+        if (baseView != null) {
+            assignPresenter(null);
+            this.baseView.initializeUI();
+        } else throw new NullPointerException();
+    }
+
+    /**
+     * initialize presenter
+     *
+     * @param presenterEnvelope bundle
+     */
+    @SuppressWarnings("unchecked")
+    private void assignPresenter(final @Nullable Bundle presenterEnvelope) {
+        if (this.presenter == null) {
+            final RequirePresenter annotation = getClass().getAnnotation(RequirePresenter.class);
+            final Class<Presenter> presenterClass =
+                    annotation == null ? null : (Class<Presenter>) annotation.value();
+            if (presenterClass != null) {
+                this.presenter =
+                        ActivityPresenterManager.Companion.getInstance()
+                                .fetch(
+                                        this,
+                                        presenterClass,
+                                        BundleUtils.INSTANCE.maybeGetBundle(presenterEnvelope, PRESENTER_KEY));
+                this.presenter.setBaseView(baseView);
+            }
+        }
+    }
+}
