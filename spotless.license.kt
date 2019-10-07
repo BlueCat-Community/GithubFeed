86a73f0dd1e8cb0b5b07c46f3a27481c5d916d3c
rurimo <topmazino@gmail.com>
2019-10-07 21:14:17 +0900

    Add spotless for ktlint & indentation

--- spotless.license.kt
+++ spotless.license.kt
@@ -14,15 +14,3 @@
  * limitations under the License.
  */
 
-package com.bluecat.githubfeed
-
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-
-class MainActivity : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
-}
