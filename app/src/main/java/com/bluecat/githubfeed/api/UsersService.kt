f792bef331b31baafe33e509d18fea7cc19f5047
tlaabs <tlaabs@naver.com>
2019-10-13 23:56:41 +0900

    create LoginActivity-layout

--- app/src/main/java/com/bluecat/githubfeed/api/UsersService.kt
+++ app/src/main/java/com/bluecat/githubfeed/api/UsersService.kt
@@ -26,4 +26,6 @@ interface UsersService {
     // https://developer.github.com/v3/users/
     @GET("/users/{username}")
     fun fetchUser(@Path("username") username: String): LiveData<ApiResponse<GithubUser>>
+
+
 }
